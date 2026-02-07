0a48487226fe8c925ebea7c89cb3e111b8d8abaa
luisar0602-netizen <luisar0602@gmail.com>
2026-02-07 15:55:17 -0500

    Nueva subida

---
+++ Pruebas semana2/src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+public class Main {
+    public static void main(String[] args){
+        String nombre = "Nayla";
+        int edad = 17 ;
+        boolean mujer = true;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(mujer);
+
+        int anioActual = 2026;
+        final int ANIONACIMIENTO = 2008;
+        int actualEdad = anioActual - ANIONACIMIENTO;
+        System.out.println(actualEdad);
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+
+    }
+}
